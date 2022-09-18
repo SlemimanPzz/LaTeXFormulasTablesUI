4b09c63db8512d0bb92a5d4d657cca8c5220902b
Ricardo Emiliano Apodaca Cardiel <emilianoa@ciencias.unam.mx>
2022-09-17 23:57:02 -0500

    "Some Minur Stuff"

---
+++ LaTeX Logic Formulas/LogicHandler/Formulas.swift
@@ -0,0 +1,24 @@
+//
+//  Formulas.swift
+//  LaTeX Logic Formulas
+//
+//  Created by Emiliano Apodaca on 17/09/22.
+//
+
+import Foundation
+
+
+enum LogicConnectors {
+    case Conguntion
+    case Disyuntion
+    case Implication
+    case Negation
+    case Biconditional
+}
+
+class Formulas {
+    init(left : Int, connector : LogicConnectors, right : Int = 0) {
+        
+    }
+    
+}
